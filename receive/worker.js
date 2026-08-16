688c4ca25fe0a90645326493293be24f3908a4ee
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-16 00:24:25 -0400

    add CRC-gated sparse tracked hot path

--- receive/worker.js
+++ receive/worker.js
@@ -148,11 +148,11 @@ function decodeNativeBatch(zx, ptr, width, height, ox, oy, tracks, pixelFormat =
     rsFallbacks: view.getUint32(nativeMetricsPtr + 68, true),
     anchorSuccesses: view.getUint32(nativeMetricsPtr + 72, true),
     anchorMisses: view.getUint32(nativeMetricsPtr + 76, true),
-    alignmentFitAttempts: view.getUint32(nativeMetricsPtr + 80, true),
+    fastSamplerAttempts: view.getUint32(nativeMetricsPtr + 80, true),
     outOfFrameMisses: view.getUint32(nativeMetricsPtr + 84, true),
     bitstreamFailures: view.getUint32(nativeMetricsPtr + 88, true),
     crcFailures: view.getUint32(nativeMetricsPtr + 92, true),
-    alignmentFitSuccesses: view.getUint32(nativeMetricsPtr + 96, true),
+    fastSamplerSuccesses: view.getUint32(nativeMetricsPtr + 96, true),
     anchorBypassAttempts: view.getUint32(nativeMetricsPtr + 100, true),
     anchorBypassSuccesses: view.getUint32(nativeMetricsPtr + 104, true)
   };
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 (() => {
-  const CACHE = "airgapper-static-js-v36";
+  const CACHE = "airgapper-static-js-v37";
   const PRECACHE = [
     "./main.js",
     "./icon-192.png",
