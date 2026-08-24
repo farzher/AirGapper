270c1346ff5fe08a052b4086f3388219d8db67b3
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-23 20:46:12 -0400

    v0.5.393: expose RaptorQ packet ESI

--- shared/coding-mode.js
+++ shared/coding-mode.js
@@ -4,9 +4,14 @@ const RAPTOR_MAX_K = 56403;
 function codingMode(k) {
   return k <= 1 ? "direct" : k <= MDS_MAX_K ? "mds" : "raptorq";
 }
+function raptorPacketEsi(packet) {
+  if (!(packet instanceof Uint8Array) || packet.length < RAPTOR_PACKET_ID_BYTES || packet[0] !== 0) return -1;
+  return packet[1] * 65536 + packet[2] * 256 + packet[3];
+}
 export {
   MDS_MAX_K,
   RAPTOR_MAX_K,
   RAPTOR_PACKET_ID_BYTES,
-  codingMode
+  codingMode,
+  raptorPacketEsi
 };
