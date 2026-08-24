9b505941d8d30029cda0fb4fc0f2e08ade6f5083
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-24 01:24:07 -0400

    Install low-allocation TrackProcessor watchdog first

--- receive/main.js
+++ receive/main.js
@@ -1,5 +1,6 @@
 import "./camera-constraints.js";
 import "./camera-constraint-guard.js";
+import "./track-processor-watchdog.js";
 import "./runtime-guards.js";
 import "./lattice-cache-guard.js";
 import "./worker-capacity-guard.js";
