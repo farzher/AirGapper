8fa0192857004c8f5322a2c194cd33d872c1cf13
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-24 00:59:03 -0400

    load lattice snapshot cache guard

--- receive/main.js
+++ receive/main.js
@@ -1,5 +1,6 @@
 import "./camera-constraints.js";
 import "./camera-constraint-guard.js";
 import "./runtime-guards.js";
+import "./lattice-cache-guard.js";
 import "./worker-capacity-guard.js";
 import "./runtime.js";
