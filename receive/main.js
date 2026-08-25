15e29194649954263a534d9e17302d493a0c10d2
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-24 21:33:40 -0400

    remove redundant stability layer

--- receive/main.js
+++ receive/main.js
@@ -8,7 +8,6 @@ import "./runtime-guards.js";
 import "./acquisition-focus-recovery.js";
 import "./lattice-cache-guard.js";
 import "./worker-capacity-guard.js";
-import "./stability-guard.js";
 import "./timeout-diagnostics.js";
 import "./dev-settings-unlock.js";
 import "./status-neutral.js";
