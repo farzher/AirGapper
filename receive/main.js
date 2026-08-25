d4ae47374d2a650c094c966019c7d961a611f837
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-24 21:12:31 -0400

    load QR exposure latch guard

--- receive/main.js
+++ receive/main.js
@@ -2,6 +2,7 @@ import "./camera-constraints.js";
 import "./camera-constraint-guard.js";
 import "./exposure-ev.js";
 import "./auto-optics-exposure-ceiling.js";
+import "./qr-exposure-latch-guard.js";
 import "./track-processor-proxy.js";
 import "./overlay-coordinate-guard.js";
 import "./track-processor-watchdog.js";
