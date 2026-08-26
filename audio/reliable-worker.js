30aea5fc670004425ff3efeae91ae6d9200170d3
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-26 11:20:10 -0400

    Isolate reliable audio decoding

--- audio/reliable-worker.js
+++ audio/reliable-worker.js
@@ -1,5 +1,4 @@
 import { ReliableScanner } from "./modem.js";
-import { QuietScanner } from "./quiet-modem.js";
 
 function sendPacket(packet) {
   const block = packet.block.slice();
@@ -9,16 +8,12 @@ function sendPacket(packet) {
 const reliable = new ReliableScanner(sendPacket, (quality) => {
   postMessage({ type: "signal", quality });
 });
-const quiet = new QuietScanner(sendPacket);
 
 self.onmessage = (event) => {
   const message = event.data;
   if (message?.type === "samples" && message.samples instanceof ArrayBuffer) {
-    const samples = new Float32Array(message.samples);
-    reliable.append(samples);
-    quiet.append(samples);
+    reliable.append(new Float32Array(message.samples));
   } else if (message?.type === "reset") {
     reliable.reset();
-    quiet.reset();
   }
 };
