c200fcc3364887fc52baf0534c8adcdf6e3202b9
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-26 10:17:43 -0400

    Add Cyrinx fast audio worker

---
+++ audio/fast-worker.js
@@ -0,0 +1,20 @@
+import { FastScanner } from "./fast-modem.js";
+
+const scanner = new FastScanner((packets) => {
+  const transfers = [];
+  const serialized = packets.map((packet) => {
+    const block = packet.block.slice();
+    transfers.push(block.buffer);
+    return { ...packet, block: block.buffer };
+  });
+  postMessage({ type: "packets", packets: serialized }, transfers);
+});
+
+self.onmessage = (event) => {
+  const message = event.data;
+  if (message?.type === "samples" && message.samples instanceof ArrayBuffer) {
+    scanner.append(new Float32Array(message.samples));
+  } else if (message?.type === "reset") {
+    scanner.reset();
+  }
+};
