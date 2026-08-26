e781f7e2b98d7dbc00214dfa2689e4428f7389e6
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-26 15:33:14 -0400

    bump version to 0.5.554

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 import "./audio/output-level.js";
 
-export const APP_VERSION = "0.5.553";
+export const APP_VERSION = "0.5.554";
 export const APP_BUILD = `v${APP_VERSION}`;
 
 let updateCheckPending = false;
