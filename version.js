1e3c02525da8e7f12378e1734dda803fef7dbb8b
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-27 00:02:35 -0400

    vendor ggwave for Reliable audio

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 import "./audio/output-level.js";
 
-export const APP_VERSION = "0.5.562";
+export const APP_VERSION = "0.5.563";
 export const APP_BUILD = `v${APP_VERSION}`;
 
 let updateCheckPending = false;
