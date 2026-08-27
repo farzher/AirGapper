8e542a55ec6f8f373a92d5b60b98d5af74c60e1f
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-27 15:59:11 -0400

    fix Reliable RaptorQ packet ids

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 import "./audio/output-level.js";
 
-export const APP_VERSION = "0.5.574";
+export const APP_VERSION = "0.5.575";
 export const APP_BUILD = `v${APP_VERSION}`;
 
 let updateCheckPending = false;
