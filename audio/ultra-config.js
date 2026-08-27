5369ab712d8fbf082c726b8203980a53d2466626
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-27 13:07:05 -0400

    configure fixed Reliable ggwave framing

---
+++ audio/ultra-config.js
@@ -0,0 +1,11 @@
+const ULTRA_MESSAGE_LENGTH = 55;
+const GGWAVE_DSS = 1 << 4;
+const GGWAVE_RX = 1 << 1;
+const GGWAVE_TX = 1 << 2;
+
+export {
+  GGWAVE_DSS,
+  GGWAVE_RX,
+  GGWAVE_TX,
+  ULTRA_MESSAGE_LENGTH
+};
