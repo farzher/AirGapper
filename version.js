4b3c47eddba443081bc8954a2cacc3bed1110377
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-27 19:55:02 -0400

    bump AirGapper to v0.5.578

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 import "./audio/output-level.js";
 
-export const APP_VERSION = "0.5.577";
+export const APP_VERSION = "0.5.578";
 export const APP_BUILD = `v${APP_VERSION}`;
 
 let updateCheckPending = false;
