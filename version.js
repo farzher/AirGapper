a2a2df88c7ccfbcf1ae8d6b1d09c8f5ed26d1c02
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-08-27 15:24:25 -0400

    fix Reliable ggwave protocol selection

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 import "./audio/output-level.js";
 
-export const APP_VERSION = "0.5.573";
+export const APP_VERSION = "0.5.574";
 export const APP_BUILD = `v${APP_VERSION}`;
 
 let updateCheckPending = false;
